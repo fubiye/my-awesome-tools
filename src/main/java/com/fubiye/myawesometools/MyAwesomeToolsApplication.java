6df251f07998cdd0ecb1a60bda05881f296c0ff7
Fu, Biye <biye.fu@outlook.com>
2019-07-07 18:32:16 +0800

    Init commit

---
+++ src/main/java/com/fubiye/myawesometools/MyAwesomeToolsApplication.java
@@ -0,0 +1,13 @@
+package com.fubiye.myawesometools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAwesomeToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAwesomeToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fubiye/myawesometools/MyAwesomeToolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fubiye.myawesometools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyAwesomeToolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
